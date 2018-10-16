3584a95585e15334c2d2d601f3c0a8f8d9a9cfcc
Explorer <propokerstar90@gmail.com>
2018-10-16 22:37:44 +0300

    Init

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 function f(x) { // function
-    console.time('Execution time took');
+    console.time('Switch statement');
     let text = `switch(x){`;
     for(let z = 0; z < 1000; z++){
         text+=`case ${z}:alert("true");break;`;
     }
     text+=`}`;
     eval(text);
-    console.timeEnd("Execution time took");
+    console.timeEnd("Switch statement");
 }
-f(999);//true
-//f(1001); //nothing
+f(1);//true
+
+function f1(x) {
+    console.time('if else statement');
+    let text;
+    for(let z = 0; z < 1000; z++){
+        text = `if(x === ${z}){
+            alert(${z})
+        }`
+    }
+    text+=`else if(x){
+        false
+    }`;
+    eval(text);
+    console.timeEnd("if else statement");
+}
+
+f1(999);
